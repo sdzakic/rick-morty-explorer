bab8c18e293d0d7924225fd952fd69d37cf4df44
Slobodan Džakić <slobodan.dzakic@gmail.com>
2026-06-08 10:15:00 +0200

    Inicijalno postavljanje Vue 3 + Vite projekta

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import AppHeader from '@/components/AppHeader.vue'
+</script>
+
+<template>
+  <div class="min-h-screen">
+    <AppHeader />
+    <main><RouterView /></main>
+  </div>
+</template>
